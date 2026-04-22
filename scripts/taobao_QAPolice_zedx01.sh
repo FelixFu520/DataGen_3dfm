62b14010f6abd1461e9287276973f2cb3913e377
fa.fu <alanmathisonturing@163.com>
2026-04-22 16:10:30 +0800

    QAPoliceStation script

---
+++ scripts/taobao_QAPolice_zedx01.sh
@@ -0,0 +1,18 @@
+cd "$(dirname "$0")/.."
+
+./app/python.sh gen_data.py \
+--scene_usd_url /root/vepfs/isaacsim/DataGen_3dfm/asset_extern/TaoBao/QAPoliceStation/OA_PoliceStation_Demo.usd \
+--camera_usd_url /root/vepfs/isaacsim/DataGen_3dfm/assets/zedx01.usd \
+--output_dir /root/vepfs/isaacsim/workdir/3dfm/taobao_QAPoliceStation_zedx01 \
+--occupancy_resolution 0.1 \
+--num_points 20 \
+--num_paths 150 \
+--max_angle_deviation 10.0 \
+--erode_iterations 2 \
+--wall_dilate_iterations 2 \
+
+./app/python.sh show_data.py \
+--data_dir /root/vepfs/isaacsim/workdir/3dfm/taobao_QAPoliceStation_zedx01 \
+--save_dir /root/vepfs/isaacsim/workdir/3dfm/taobao_QAPoliceStation_zedx01/vis \
+--show_num 4
+
