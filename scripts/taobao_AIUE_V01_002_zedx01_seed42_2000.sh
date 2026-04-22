8f5c1f9ca35ac9aa63e34d92bf424a8261526d5a
fa.fu <alanmathisonturing@163.com>
2026-04-22 20:55:28 +0800

    AIUE_V01_*执行脚本

---
+++ scripts/taobao_AIUE_V01_002_zedx01_seed42_2000.sh
@@ -0,0 +1,19 @@
+cd "$(dirname "$0")/.."
+
+./app/python.sh gen_data.py \
+--seed 42 \
+--scene_usd_url /root/vepfs/isaacsim/DataGen_3dfm/asset_extern/TaoBao/AIUE_V01_002/AIUE_V01_002.usd \
+--camera_usd_url /root/vepfs/isaacsim/DataGen_3dfm/assets/zedx01.usd \
+--output_dir /root/vepfs/isaacsim/workdir/3dfm/taobao_AIUE_V01_002_zedx01_seed42_2000 \
+--occupancy_resolution 0.5 \
+--num_points 20 \
+--num_paths 150 \
+--max_angle_deviation 10.0 \
+--erode_iterations 2 \
+--wall_dilate_iterations 2 \
+
+./app/python.sh show_data.py \
+--data_dir /root/vepfs/isaacsim/workdir/3dfm/taobao_AIUE_V01_002_zedx01_seed42_2000 \
+--save_dir /root/vepfs/isaacsim/workdir/3dfm/taobao_AIUE_V01_002_zedx01_seed42_2000/vis \
+--show_num 4
+
---
+++ submit.sh
@@ -0,0 +1,23 @@
+# echo $VOLC_AK
+# echo $VOLC_SK
+# echo $VOLC_PASSWD
+
+# ============================== AIUE_V01_002
+# 已执行
+# ./app/python.sh submit_volcengine.py --ak "${VOLC_AK}" --sk "${VOLC_SK}" --private_image_password "${VOLC_PASSWD}"  \
+# --task_name taobao_AIUE_V01_002_zedx01_seed42_2000 \
+# --command  /root/vepfs/isaacsim/DataGen_3dfm/scripts/taobao_AIUE_V01_002_zedx01_seed42_2000.sh 
+
+
+# ============================== AIUE_V01_004
+# 已执行
+# ./app/python.sh submit_volcengine.py --ak "${VOLC_AK}" --sk "${VOLC_SK}" --private_image_password "${VOLC_PASSWD}"  \
+# --task_name taobao_AIUE_V01_004_zedx01_seed42_2000 \
+# --command  /root/vepfs/isaacsim/DataGen_3dfm/scripts/taobao_AIUE_V01_004_zedx01_seed42_2000.sh 
+
+
+# ============================== AIUE_V01_005
+# 已执行
+# ./app/python.sh submit_volcengine.py --ak "${VOLC_AK}" --sk "${VOLC_SK}" --private_image_password "${VOLC_PASSWD}"  \
+# --task_name taobao_AIUE_V01_005_zedx01_seed42_2000 \
+# --command  /root/vepfs/isaacsim/DataGen_3dfm/scripts/taobao_AIUE_V01_005_zedx01_seed42_2000.sh 
