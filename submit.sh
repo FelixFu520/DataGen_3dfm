be6e1f904e0f75ed6fc959c22cfefd80d1e96dec
fa.fu <alanmathisonturing@163.com>
2026-04-22 21:39:40 +0800

    add script

--- submit.sh
+++ submit.sh
@@ -54,6 +54,12 @@
 
 # ============================== NightClub, 场景较大
 # 已执行
-./app/python.sh submit_volcengine.py --ak "${VOLC_AK}" --sk "${VOLC_SK}" --private_image_password "${VOLC_PASSWD}"  \
---task_name taobao_NightClub_zedx01_seed42_2000 \
---command  /root/vepfs/isaacsim/DataGen_3dfm/scripts/taobao_NightClub_zedx01_seed42_2000.sh 
+# ./app/python.sh submit_volcengine.py --ak "${VOLC_AK}" --sk "${VOLC_SK}" --private_image_password "${VOLC_PASSWD}"  \
+# --task_name taobao_NightClub_zedx01_seed42_2000 \
+# --command  /root/vepfs/isaacsim/DataGen_3dfm/scripts/taobao_NightClub_zedx01_seed42_2000.sh 
+
+# ============================== OfficeMeetingRoom2, 场景适小
+# 已执行
+# ./app/python.sh submit_volcengine.py --ak "${VOLC_AK}" --sk "${VOLC_SK}" --private_image_password "${VOLC_PASSWD}"  \
+# --task_name taobao_OfficeMeetingRoom2_zedx01_seed42_2000 \
+# --command  /root/vepfs/isaacsim/DataGen_3dfm/scripts/taobao_OfficeMeetingRoom2_zedx01_seed42_2000.sh 
