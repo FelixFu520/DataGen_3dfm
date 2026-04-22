9e058e92ba155a2898a174e0269fe2fd5b98c7ab
fa.fu <alanmathisonturing@163.com>
2026-04-22 19:56:30 +0800

    修改script

--- scripts/home_007_zedx01_seed42_10000.sh
+++ scripts/home_007_zedx01_seed42_10000.sh
@@ -3,16 +3,16 @@ cd "$(dirname "$0")/.."
 ./app/python.sh gen_data.py \
 --scene_usd_url /root/vepfs/isaacsim/DataGen_3dfm/asset_extern/Scene-Home-Issac/home_007/home_007.usdc \
 --camera_usd_url /root/vepfs/isaacsim/DataGen_3dfm/assets/zedx01.usd \
---output_dir /root/vepfs/isaacsim/workdir/3dfm/home007_zedx01 \
+--output_dir /root/vepfs/isaacsim/workdir/3dfm/home007_zedx01_seed42_10000 \
 --occupancy_resolution 0.1 \
 --num_points 20 \
---num_paths 100 \
+--num_paths 600 \
 --max_angle_deviation 10.0 \
 --erode_iterations 2 \
 --wall_dilate_iterations 2 \
 
 ./app/python.sh show_data.py \
---data_dir /root/vepfs/isaacsim/workdir/3dfm/home007_zedx01 \
---save_dir /root/vepfs/isaacsim/workdir/3dfm/home007_zedx01/vis \
+--data_dir /root/vepfs/isaacsim/workdir/3dfm/home007_zedx01_seed42_10000 \
+--save_dir /root/vepfs/isaacsim/workdir/3dfm/home007_zedx01_seed42_10000/vis \
 --show_num 4
 
