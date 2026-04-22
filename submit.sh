1d58276e16bb49d82363cf8a793c5d23bacdd792
fa.fu <alanmathisonturing@163.com>
2026-04-22 21:50:06 +0800

    add script

--- submit.sh
+++ submit.sh
@@ -73,6 +73,12 @@
 
 # ============================== OutdoorFurniture, 场景中
 # 已执行
+# ./app/python.sh submit_volcengine.py --ak "${VOLC_AK}" --sk "${VOLC_SK}" --private_image_password "${VOLC_PASSWD}"  \
+# --task_name taobao_OutdoorFurniture_zedx01_seed42_2000 \
+# --command  /root/vepfs/isaacsim/DataGen_3dfm/scripts/taobao_OutdoorFurniture_zedx01_seed42_2000.sh 
+
+# ============================== ParkingGarage, 场景大
+# 已执行
 ./app/python.sh submit_volcengine.py --ak "${VOLC_AK}" --sk "${VOLC_SK}" --private_image_password "${VOLC_PASSWD}"  \
---task_name taobao_OutdoorFurniture_zedx01_seed42_2000 \
---command  /root/vepfs/isaacsim/DataGen_3dfm/scripts/taobao_OutdoorFurniture_zedx01_seed42_2000.sh 
+--task_name taobao_ParkingGarage_zedx01_seed42_2000 \
+--command  /root/vepfs/isaacsim/DataGen_3dfm/scripts/taobao_ParkingGarage_zedx01_seed42_2000.sh 
