8800d9964fa0de5cb8b6e9d44cdba45b69e6b60e
fa.fu <alanmathisonturing@163.com>
2026-04-22 21:08:41 +0800

    两个任务脚本

--- submit.sh
+++ submit.sh
@@ -27,3 +27,15 @@
 # ./app/python.sh submit_volcengine.py --ak "${VOLC_AK}" --sk "${VOLC_SK}" --private_image_password "${VOLC_PASSWD}"  \
 # --task_name taobao_AIUE_V03_001_zedx01_seed42_2000 \
 # --command  /root/vepfs/isaacsim/DataGen_3dfm/scripts/taobao_AIUE_V03_001_zedx01_seed42_2000.sh 
+
+# ============================== AIUE_V03_002
+# 已执行
+# ./app/python.sh submit_volcengine.py --ak "${VOLC_AK}" --sk "${VOLC_SK}" --private_image_password "${VOLC_PASSWD}"  \
+# --task_name taobao_AIUE_V03_002_zedx01_seed42_2000 \
+# --command  /root/vepfs/isaacsim/DataGen_3dfm/scripts/taobao_AIUE_V03_002_zedx01_seed42_2000.sh 
+
+# ============================== AI_vol33_scene_04
+# 已执行
+# ./app/python.sh submit_volcengine.py --ak "${VOLC_AK}" --sk "${VOLC_SK}" --private_image_password "${VOLC_PASSWD}"  \
+# --task_name taobao_AI_vol33_scene_04_zedx01_seed42_2000 \
+# --command  /root/vepfs/isaacsim/DataGen_3dfm/scripts/taobao_AI_vol33_scene_04_zedx01_seed42_2000.sh 
