22a9c449e0aa5f1c62fed5d4ee5087d5c7c8156e
fa.fu <alanmathisonturing@163.com>
2026-04-22 22:03:26 +0800

    add script

--- submit.sh
+++ submit.sh
@@ -94,3 +94,9 @@
 # ./app/python.sh submit_volcengine.py --ak "${VOLC_AK}" --sk "${VOLC_SK}" --private_image_password "${VOLC_PASSWD}"  \
 # --task_name taobao_PostSovietKitchen_zedx01_seed42_2000 \
 # --command  /root/vepfs/isaacsim/DataGen_3dfm/scripts/taobao_PostSovietKitchen_zedx01_seed42_2000.sh 
+
+# ============================== QAModularParking, 场景大
+# 已执行
+# ./app/python.sh submit_volcengine.py --ak "${VOLC_AK}" --sk "${VOLC_SK}" --private_image_password "${VOLC_PASSWD}"  \
+# --task_name taobao_QAModularParking_zedx01_seed42_2000 \
+# --command  /root/vepfs/isaacsim/DataGen_3dfm/scripts/taobao_QAModularParking_zedx01_seed42_2000.sh 
