41811e7f7a9f62ca219429eb64573e3f1c31e405
fa.fu <alanmathisonturing@163.com>
2026-04-23 15:22:20 +0800

    add script

--- submit01.sh
+++ submit01.sh
@@ -25,7 +25,8 @@ TASKS=(
   # taobao_ModularSwimmingPool_zedx01_seed42_2000  # 已执行
   # taobao_NewScandinavian_zedx01_seed42_2000  # 已执行
   # taobao_NightClub_zedx01_seed42_2000  # 已执行
-  # taobao_OfficeMeetingRoom2_zedx01_seed42_2000  # 已执行 
+  # taobao_OfficeMeetingRoom2_zedx01_seed42_2000  # 已执行
+  # taobao_Old_Laboratory2_zedx01_seed42_2000  # 已执行
 )
 
 for task in "${TASKS[@]}"; do
