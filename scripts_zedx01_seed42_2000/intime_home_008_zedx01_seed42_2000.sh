5e495157cb69cd916a7eb1b016bad11610c017fb
fa.fu <alanmathisonturing@163.com>
2026-04-23 14:06:38 +0800

    修改脚本

---
+++ scripts_zedx01_seed42_2000/intime_home_008_zedx01_seed42_2000.sh
@@ -0,0 +1,22 @@
+cd "$(dirname "$0")/.."
+
+./app/python.sh gen_data.py \
+--seed 42 \
+--scene_usd_url /root/vepfs/isaacsim/DataGen_3dfm/asset_extern/Intime_Home/home_008/home_008.usdc \
+--camera_usd_url /root/vepfs/isaacsim/DataGen_3dfm/assets/zedx01.usd \
+--output_dir /root/vepfs/isaacsim/workdir/3dfm_zedx01_seed42_2000/home008_zedx01_seed42_2000 \
+--occupancy_resolution 0.1 \
+--num_points 100 \
+--num_paths 20 \
+--max_angle_deviation 10.0 \
+--erode_iterations 2 \
+--wall_dilate_iterations 2 \
+--step_size_xy 0.3 \
+--step_size_z 0.1 \
+--max_dz_per_step 0.1
+
+./app/python.sh show_data.py \
+--data_dir /root/vepfs/isaacsim/workdir/3dfm_zedx01_seed42_2000/home008_zedx01_seed42_2000 \
+--save_dir /root/vepfs/isaacsim/workdir/3dfm_zedx01_seed42_2000/home008_zedx01_seed42_2000/vis \
+--show_num 4
+
--- submit.sh
+++ submit.sh
@@ -1,22 +1,19 @@
-SCRIPT_DIR="/root/vepfs/isaacsim/DataGen_3dfm/scripts_seed42_2000"
+SCRIPT_DIR="/root/vepfs/isaacsim/DataGen_3dfm/scripts_zedx01_seed42_2000"
 
 # 每项为 task_name，对应脚本为 ${SCRIPT_DIR}/${task}.sh
 TASKS=(
-  taobao_AIUE_V03_001_zedx01_seed42_2000        # AIUE_V03_001, 场景较大, 可以多跑些数据
-  taobao_AIUE_V03_002_zedx01_seed42_2000        # AIUE_V03_002, 场景较大, 可以多跑些数据
-  taobao_AI_vol33_scene_04_zedx01_seed42_2000   # AI_vol33_scene_04, 场景较大, 可以多跑些数据
-  taobao_ModularSwimmingPool_zedx01_seed42_2000 # ModularSwimmingPool, 场景适中
-  taobao_NewScandinavian_zedx01_seed42_2000     # NewScandinavian, 场景较小
-  taobao_NightClub_zedx01_seed42_2000           # NightClub, 场景较大
-  taobao_OfficeMeetingRoom2_zedx01_seed42_2000  # OfficeMeetingRoom2, 场景小
-  taobao_Old_Laboratory2_zedx01_seed42_2000     # Old_Laboratory2, 场景小
-  taobao_OutdoorFurniture_zedx01_seed42_2000    # OutdoorFurniture, 场景中
-  taobao_ParkingGarage_zedx01_seed42_2000       # ParkingGarage, 场景大
-  taobao_PostSovietFlat2_zedx01_seed42_2000     # PostSovietFlat2, 场景小
-  taobao_PostSovietKitchen_zedx01_seed42_2000   # PostSovietKitchen, 场景小
-  taobao_QAModularParking_zedx01_seed42_2000    # QAModularParking, 场景大
-  taobao_QAOffice_zedx01_seed42_2000            # QAOffice, 场景中
-  taobao_QAPolice_zedx01_seed42_2000            # QAPolice, 场景大
+  # Intime_Home场景
+  # intime_home_000_zedx01_seed42_2000
+  intime_home_001_zedx01_seed42_2000
+  intime_home_002_zedx01_seed42_2000
+  intime_home_003_zedx01_seed42_2000
+  intime_home_004_zedx01_seed42_2000
+  intime_home_005_zedx01_seed42_2000
+  intime_home_006_zedx01_seed42_2000
+  intime_home_007_zedx01_seed42_2000
+  intime_home_008_zedx01_seed42_2000
+  intime_home_009_zedx01_seed42_2000
+  intime_home_010_zedx01_seed42_2000
 )
 
 for task in "${TASKS[@]}"; do
