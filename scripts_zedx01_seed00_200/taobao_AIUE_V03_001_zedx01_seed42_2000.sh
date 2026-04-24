9845e285a387d6c729193f3f6e35ed8117812f09
felixfu <alanmathisonturing@163.com>
2026-04-24 01:46:53 +0000

    add script

---
+++ scripts_zedx01_seed00_200/taobao_AIUE_V03_001_zedx01_seed42_2000.sh
@@ -0,0 +1,23 @@
+cd "$(dirname "$0")/.."
+
+# 场景是个小区， 但是只把房子设置了碰撞属性，因此场景大
+./app/python.sh gen_data.py \
+--seed 42 \
+--scene_usd_url /root/vepfs/isaacsim/DataGen_3dfm/asset_extern/TaoBao/AIUE_V03_001/AI_vol3_scene_01.usd \
+--camera_usd_url /root/vepfs/isaacsim/DataGen_3dfm/assets/zedx01.usd \
+--output_dir /root/vepfs/isaacsim/workdir/3dfm_zedx01_seed42_2000/taobao_AIUE_V03_001_zedx01_seed42_2000 \
+--occupancy_resolution 0.25 \
+--num_points 100 \
+--num_paths 20 \
+--max_angle_deviation 10.0 \
+--erode_iterations 2 \
+--obstacle_dilate_iterations 2 \
+--obstacle_envelope_iterations 8 \
+--step_size_xy 0.3 \
+--step_size_z 0.1 \
+--max_dz_per_step 0.1
+
+./app/python.sh show_data.py \
+--data_dir /root/vepfs/isaacsim/workdir/3dfm_zedx01_seed42_2000/taobao_AIUE_V03_001_zedx01_seed42_2000 \
+--save_dir /root/vepfs/isaacsim/workdir/3dfm_zedx01_seed42_2000/taobao_AIUE_V03_001_zedx01_seed42_2000/vis \
+--show_num 4
---
+++ scripts_zedx01_seed00_200/taobao_Old_Laboratory2_zedx01_seed42_2000.sh
@@ -0,0 +1,23 @@
+cd "$(dirname "$0")/.."
+
+# 场景是两个屋子， 中间有空白的地方， 物体都设置了碰撞属性，因此场景中
+./app/python.sh gen_data.py \
+--seed 42 \
+--scene_usd_url /root/vepfs/isaacsim/DataGen_3dfm/asset_extern/TaoBao/Old_Laboratory2/Old_Laboratory.usd \
+--camera_usd_url /root/vepfs/isaacsim/DataGen_3dfm/assets/zedx01.usd \
+--output_dir /root/vepfs/isaacsim/workdir/3dfm_zedx01_seed42_2000/taobao_Old_Laboratory2_zedx01_seed42_2000 \
+--occupancy_resolution 0.1 \
+--num_points 100 \
+--num_paths 20 \
+--max_angle_deviation 10.0 \
+--erode_iterations 2 \
+--obstacle_dilate_iterations 2 \
+--obstacle_envelope_iterations 0 \
+--step_size_xy 0.3 \
+--step_size_z 0.1 \
+--max_dz_per_step 0.1
+
+./app/python.sh show_data.py \
+--data_dir /root/vepfs/isaacsim/workdir/3dfm_zedx01_seed42_2000/taobao_Old_Laboratory2_zedx01_seed42_2000 \
+--save_dir /root/vepfs/isaacsim/workdir/3dfm_zedx01_seed42_2000/taobao_Old_Laboratory2_zedx01_seed42_2000/vis \
+--show_num 4
