38bdb4121ad3a95b2394ae60790d0c55ba309264
felixfu <alanmathisonturing@163.com>
2026-04-24 04:18:42 +0000

    intime home scripts

--- submit_seed00_200.sh
+++ submit_seed00_200.sh
@@ -3,17 +3,17 @@ SCRIPT_DIR="/root/vepfs/isaacsim/DataGen_3dfm/scripts_zedx01_seed00_200"
 # 每项为 task_name，对应脚本为 ${SCRIPT_DIR}/${task}.sh
 TASKS=(
   # Intime_Home场景
-  intime_home_000_zedx01_seed00_200  # 已执行
-  # intime_home_001_zedx01_seed00_200
-  # intime_home_002_zedx01_seed00_200
-  # intime_home_003_zedx01_seed00_200
-  # intime_home_004_zedx01_seed00_200
-  # intime_home_005_zedx01_seed00_200
-  # intime_home_006_zedx01_seed00_200
-  # intime_home_007_zedx01_seed00_200
-  # intime_home_008_zedx01_seed00_200
-  # intime_home_009_zedx01_seed00_200
-  # intime_home_010_zedx01_seed00_200
+  intime_home_000_zedx01_seed00_200
+  intime_home_001_zedx01_seed00_200
+  intime_home_002_zedx01_seed00_200
+  intime_home_003_zedx01_seed00_200
+  intime_home_004_zedx01_seed00_200
+  intime_home_005_zedx01_seed00_200
+  intime_home_006_zedx01_seed00_200
+  intime_home_007_zedx01_seed00_200
+  intime_home_008_zedx01_seed00_200
+  intime_home_009_zedx01_seed00_200
+  intime_home_010_zedx01_seed00_200
 
   # Taobao场景
   # taobao_AI_vol33_scene_04_zedx01_seed00_2000  # 已 executed
